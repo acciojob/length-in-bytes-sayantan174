793b8a9636cd91a3833500cc3f076274a9e1284c
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-10 16:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const byteSize = (str) => {
   // write your code here
-	return str.length;
+	const encoder = new TextEncoder();
+  // Encode the string into an ArrayBuffer
+  const encoded = encoder.encode(str);
+
+  // Get the byte length of the ArrayBuffer
+  const byteLength = encoded.byteLength;
+
+  return byteLength;
 };
-// Do not change the code below 
+// Do not change the code below  
 const str = prompt("Enter some string.");
 alert(byteSize(str));
